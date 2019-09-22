12743000576f26a2aa76bf577b10d5120237b986
capjie <13777857953@163.com>
2019-09-22 10:10:35 +0800

    PY作业

--- homework.py
+++ homework.py
@@ -67,3 +67,38 @@ map(function, iterable, ...)
 # print(type(num1))
 # sum1 = sum(num1)#只会将int类型的数据相加，参数可以是：列表，集合，元组
 # print(sum1)
+
+
+
+# 编写功能函数num=function_name(input)
+# 统计下面这句话The Zen of Python中，单词'is'出现的次数
+# 提示，help(str.split)
+
+zen = "The Zen of Python, by Tim Peters\
+Beautiful is better than ugly.\
+Explicit is better than implicit.\
+Simple is better than complex.\
+Complex is better than complicated.\
+Flat is better than nested.\
+Sparse is better than dense.\
+Readability counts.\
+Special cases aren't special enough to break the rules.\
+Although practicality beats purity.\
+Errors should never pass silently.\
+Unless explicitly silenced.\
+In the face of ambiguity, refuse the temptation to guess.\
+There should be one-- and preferably only one --obvious way to do it.\
+Although that way may not be obvious at first unless you're Dutch.\
+Now is better than never.\
+Although never is often better than *right* now.\
+If the implementation is hard to explain, it's a bad idea.\
+If the implementation is easy to explain, it may be a good idea.\
+Namespaces are one honking great idea -- let's do more of those!"
+
+def Statistics(str1):
+    sub = 'is'
+    return str1.count(sub)
+
+if __name__ =='__main__':
+    num=Statistics(zen)
+    print('is出现的次数:',num)
